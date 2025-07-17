76c14528940174c876b7535c16cd1cef01c04d0c
Shardbyte <saint@shardbyte.com>
2025-07-17 18:03:11 -0500

    update entrypoint

--- images/arkmanager/entrypoint.sh
+++ images/arkmanager/entrypoint.sh
@@ -5,13 +5,20 @@
 #################################################
 # base installation script
 
-if [ -e "stop-install-loop.sh" ]; then
-    echo "Installation already completed, skipping..." >&2
+sleep 1
+
+TZ=${TZ:-UTC}
+export TZ
+
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+if command -v arkmanager >/dev/null 2>&1; then
+    echo "Ark Server Tools already installed, skipping..." >&2
 else
     echo "Installing Ark Server Tools..."
     if curl -sL https://raw.githubusercontent.com/arkmanager/ark-server-tools/master/netinstall.sh | bash -s container --me --perform-user-install --yes-i-really-want-to-perform-a-user-install; then
         if cp /home/container/bin/arkmanager /home/container/arkmanager; then
-            echo "Creating install completion marker..." | tee stop-install-loop.sh > /dev/null
             echo "Ark Server Tools installation completed successfully"
         else
             echo "Failed to copy arkmanager binary" >&2
@@ -21,6 +28,23 @@ else
         echo "Failed to install Ark Server Tools" >&2
         exit 1
     fi
+fi
+
+cd /home/container || exit 1
+
+if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
+    if [ ! -z ${SRCDS_APPID} ]; then
+        if [ "${STEAM_USER}" == "anonymous" ]; then
+            ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update 1007 +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" )  ${INSTALL_FLAGS} $( [[ "${VALIDATE}" == "1" ]] && printf %s 'validate' ) +quit
+        else
+            ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update 1007 +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) ${INSTALL_FLAGS} $( [[ "${VALIDATE}" == "1" ]] && printf %s 'validate' ) +quit
+        fi
+    else
+        echo -e "No appid set. Starting Server"
+    fi
+
+else
+    echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
 
 MODIFIED_STARTUP=$(eval echo $(echo ./arkmanager ${STARTUP} --verbose | sed -e 's/{{/${/g' -e 's/}}/}/g'))
